a77e8a1966826fdf8ac577a3f1555f226d926ea2
Jens Krause <jens@rasta.online>
2024-01-02 16:09:40 +0700

    Update Quiz Models, remove stale DressingRoom

--- DressingRoom/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DressingroomConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DressingRoom'
--- quizzes/models.py
+++ quizzes/models.py
@@ -1,12 +1,25 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 class Quiz(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.description
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=30)
+
 
 class Answer(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     text = models.TextField()
     is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
