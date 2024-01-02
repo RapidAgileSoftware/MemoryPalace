060226a411f89e4640c19bdbe3b206926df1e00c
Jens Krause <jens@rasta.online>
2024-01-02 19:00:51 +0700

    Improve index and detail templates

--- casting/urls.py
+++ casting/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'casting'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:actor_id>/', views.detail, name='detail'),
