33190ddf085c6edc61b552c9287155300816fdc1
Jens Krause <jens@rasta.online>
2024-01-02 17:44:57 +0700

    Removed pycache from repo and corrected rehearsal view

--- casting/urls.py
+++ casting/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:actor_id>/', views.detail, name='detail'),
-    path('rehearsal/<int:actor_id>/<int:prop_id>/<int:action_id>', views.detail, name='rehearsal'),
+    path('rehearsal/<int:actor_id>/<int:prop_id>/<int:action_id>', views.rehearsal, name='rehearsal'),
 ]
