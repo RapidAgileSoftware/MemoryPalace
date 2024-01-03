acb4eb6213f6ca9ad07d6e2f9d543979f47ddf67
Jens Krause <jens@rasta.online>
2024-01-03 10:51:29 +0700

    Remove redundancies froms views

--- casting/views.py
+++ casting/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from casting.models import Actor
@@ -13,28 +12,18 @@ def index(request):
 
 def detail(request, actor_id):
     actor = get_object_or_404(Actor, pk=actor_id)
-    return render(request, 'casting/details.html', {"actor": actor})
+    return render(request, 'casting/details.html', {'actor': actor})
 
 
 def rehearsal(request):
+
+    context = {
+        'actors_list': Actor.objects.order_by(),
+    }
     if request.method == 'POST':
         try:
-            rehearsalInput = request.POST["rehearsal_input"]
+            context['rehearsal_result'] = request.POST['rehearsal_input']
         except KeyError:
-            return render(
-                request,
-                'casting/index.html',
-                {
-                    "error_message": "No rehearsal data provided.",
-                    'actors_list': Actor.objects.order_by()
-                })
-        return render(
-            request,
-            'casting/index.html',
-            {
-                "rehearsal_result": rehearsalInput,
-                'actors_list': Actor.objects.order_by()
-            }
-        )
+            context['error_message'] = 'No rehearsal data provided'
 
-    return HttpResponse(request)
+    return render(request, "casting/index.html", context)
