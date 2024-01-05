655a2dc7e999e6343ac8fca5809d9f56e024c928
Jens Krause <jens@rasta.online>
2024-01-05 12:52:57 +0700

    Introduce Image app with pretrained PyTorch Image label prediction

--- MemoryPalace/urls.py
+++ MemoryPalace/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('casting/', include('casting.urls')),
+    path('images/', include('images.urls')),
 ]
